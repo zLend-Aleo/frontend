40babd09e2a2b31d821936e37f58f2217fd1822b
andrew <a.liutiev@gmail.com>
2023-11-17 16:33:08 +0300

    first commit with ts template

---
+++ salient-ts/src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+export default async function Test() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
